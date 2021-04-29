37ed6add5d121eaa5c2291bd606626e90feb2c9b
Mark Plachetta <mark.plachetta@health.nsw.gov.au>
2021-04-30 09:55:22 +1000

    Fix livewire pagination: take 3

--- src/components/pagination/livewire-default.blade.php
+++ src/components/pagination/livewire-default.blade.php
@@ -4,14 +4,14 @@
             <ul class="nsw-pagination">
                 {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
-                    <li class="nsw-pagination__item nsw-pagination__item--prev-page">
+                    <li id="paginator-prev-disabled" class="nsw-pagination__item nsw-pagination__item--prev-page">
                         <a class="nsw-direction-link nsw-direction-link--icon-left" rel="prev">
                             <i class="material-icons nsw-material-icons nsw-material-icons--rotate-180" focusable="false" aria-hidden="true">east</i>
                             <span class="nsw-direction-link__text">Back <span class="sr-only">a page</span></span>
                         </a>
                     </li>
                 @else
-                    <li class="nsw-pagination__item nsw-pagination__item--prev-page">
+                    <li id="paginator-prev-page" class="nsw-pagination__item nsw-pagination__item--prev-page">
                         <a href="#" class="nsw-direction-link nsw-direction-link--icon-left" wire:click.prevent="previousPage" rel="prev">
                             <i class="material-icons nsw-material-icons nsw-material-icons--rotate-180" focusable="false" aria-hidden="true">east</i>
                             <span class="nsw-direction-link__text">Back <span class="sr-only">a page</span></span>
@@ -30,7 +30,7 @@
                     @if (is_array($element))
                         @foreach ($element as $page => $url)
                             @if ($page == $paginator->currentPage())
-                                <li id="paginator-current-page" class="nsw-pagination__item nsw-pagination__item--is-active" wire:key="paginator-page-{{ $page }}" aria-current="page">
+                                <li id="paginator-current-page" class="nsw-pagination__item nsw-pagination__item--is-active" wire:key="paginator-current-page" aria-current="page">
                                     <a class="nsw-pagination__link is-current">
                                         <span class="nsw-pagination__text">
                                             <span class="sr-only">Page </span>{{ $page }}
@@ -52,7 +52,7 @@
 
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
-                    <li class="nsw-pagination__item nsw-pagination__item--next-page">
+                    <li id="paginator-next-page" class="nsw-pagination__item nsw-pagination__item--next-page">
                         <a href="#" class="nsw-direction-link" wire:click.prevent="nextPage" rel="next">
                             <span class="nsw-direction-link__text">
                                 Next <span class="sr-only">page</span>
@@ -61,7 +61,7 @@
                         </a>
                     </li>
                 @else
-                    <li class="nsw-pagination__item nsw-pagination__item--next-page">
+                    <li id="paginator-next-disabled" class="nsw-pagination__item nsw-pagination__item--next-page">
                         <a class="nsw-direction-link" rel="next">
                             <span class="nsw-direction-link__text">
                                 Next <span class="sr-only">page</span>
