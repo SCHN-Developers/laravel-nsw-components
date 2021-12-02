cc679da38efcaf7f7c26ddb02dfa7ed2bf6c3854
schn-rob-tamas <robert.tamas@health.nsw.gov.au>
2021-12-02 21:44:14 +1100

    update layout component to include sidebars

--- src/components/layout/container.blade.php
+++ src/components/layout/container.blade.php
@@ -1,7 +1,18 @@
+  @props(['sidebar' => null ])
   <div {{ $attributes->merge(['class' => 'nsw-container']) }}>
-    <div class="nsw-layout">
-      <main class="nsw-layout__main">
-        {{ $slot }}
-      </main>
+    <div class="nsw-page-layout">
+        @if($sidebar == 'left')
+            <div class="nsw-page-layout__sidebar nsw-page-layout__sidebar--desktop">
+                {{ $sidebar_content }}
+            </div>
+        @endif
+        <main class="nsw-page-layout__main">
+            {{ $slot }}
+        </main>
+        @if($sidebar == 'right')
+            <div class="nsw-page-layout__sidebar">
+                {{ $sidebar_content }}
+            </div>
+        @endif
     </div>
   </div>
