3350022bc6c8e4deb3fce8967c1b5262d3f90436
schn-mark-plachetta <mark.plachetta@health.nsw.gov.au>
2022-01-18 11:41:59 +1100

    Add default value option

--- src/components/forms/select.blade.php
+++ src/components/forms/select.blade.php
@@ -6,8 +6,8 @@
     'id' => $name . '-' . Str::random(12),
     'required' => false,
     'nullOption' => false,
+    'default' => null,
 ])
-
 <div class="nsw-form__group">
 
    <label class="nsw-form__label @if($required) nsw-form__required @endif" for="{{ $id }}">{{ $label }}</label>
@@ -19,7 +19,7 @@
       <option value="">{{ $nullOption }}</option>
     @endif
     @foreach ($options as $value => $text)
-      <option value="{{ $value }}"{{ (old($name) == $value) ? ' selected' : '' }}>{{ $text }}</option>
+      <option value="{{ $value }}"{{ (old($name, $default) == $value) ? ' selected' : '' }}>{{ $text }}</option>
      @endforeach
   </select>
 
@@ -27,4 +27,3 @@
       <span id="{{ $id }}-error-text" class="nsw-form__helper nsw-form__helper--error"><i class="material-icons nsw-material-icons" focusable="false" aria-hidden="true">cancel</i>{{ $message }}</span>
   @enderror
 </div>
-
--- src/components/forms/textarea.blade.php
+++ src/components/forms/textarea.blade.php
@@ -3,7 +3,8 @@
     'label',
     'helpText',
     'id' => $name . '-' . Str::random(12),
-    'required' => false
+    'required' => false,
+    'default' => null,
 ])
 <div class="nsw-form__group">
   <label for="{{ $id }}" class="nsw-form__label @if($required) nsw-form__required @endif">{{ $label }}</label>
@@ -11,7 +12,7 @@
     <span class="nsw-form__helper" id="{{ $id }}-helper-text">{{ $helpText }}</span>
   @endif
 
-  <textarea {{ $attributes->merge(['class' => 'nsw-form__input' ]) }} id="{{ $id }}" name="{{ $name }}" @error($name) aria-invalid="true" @enderror>{{ old($name) }}</textarea>
+  <textarea {{ $attributes->merge(['class' => 'nsw-form__input' ]) }} id="{{ $id }}" name="{{ $name }}" @error($name) aria-invalid="true" @enderror>{{ old($name, $default) }}</textarea>
 
   @error ($name)
       <span class="nsw-form__helper nsw-form__helper--error" id="{{ $id }}--error-text"><i class="material-icons nsw-material-icons" focusable="false" aria-hidden="true">cancel</i>{{ $message }}</span>
